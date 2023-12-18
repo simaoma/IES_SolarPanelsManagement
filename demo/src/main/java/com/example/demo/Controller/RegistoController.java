1c04b82bc74edb9b7669bba3b58a5c656af138a8
pedromfm02 <pedromfm02@ua.pt>
2023-12-18 17:23:34 +0000

    endpontis para historico

--- demo/src/main/java/com/example/demo/Controller/RegistoController.java
+++ demo/src/main/java/com/example/demo/Controller/RegistoController.java
@@ -8,14 +8,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Entity.Registos;
@@ -38,8 +33,8 @@ public class RegistoController {
     @Autowired
     private SistemaService sistemaService;
 
-    @GetMapping("/sistema/{id}/historico/start_date={date_start}/end_date={date_end}")
-    public ResponseEntity<List<RegistoRequest>> getHistorico(@PathVariable("id") Long id_sis,@PathVariable("date_start") String date_start,@PathVariable("date_end") String date_end) {
+    @GetMapping("/sistema/{id}/historico/prod/start_date={date_start}/end_date={date_end}")
+    public ResponseEntity<List<RegistoRequest>> getHistoricoProd(@PathVariable("id") Long id_sis,@PathVariable("date_start") String date_start,@PathVariable("date_end") String date_end) {
         
         System.out.println("id: " +id_sis+ ", start: " +date_start+ ", end: "+date_end);
         String date_start_str = date_start.replace("\"", "").trim();
@@ -56,8 +51,38 @@ public class RegistoController {
         Iterator<Registos> iterator = regEndList.iterator();
         while (iterator.hasNext()) {
             Registos reg = iterator.next();
-            RegistoRequest reg_req = new RegistoRequest(reg.getEnergia(), reg.getTime_init(), reg.getTime_final(), reg.getType());
-            request_list.add(reg_req);
+            if(reg.getType() == "Prod"){
+                RegistoRequest reg_req = new RegistoRequest(reg.getEnergia(), reg.getTime_init(), reg.getTime_final(), reg.getType());
+                request_list.add(reg_req);
+            }
+        }
+
+        return  new ResponseEntity<>(request_list,HttpStatus.OK);
+    }
+
+    @GetMapping("/sistema/{id}/historico/cons/start_date={date_start}/end_date={date_end}")
+    public ResponseEntity<List<RegistoRequest>> getHistoricoCons(@PathVariable("id") Long id_sis,@PathVariable("date_start") String date_start,@PathVariable("date_end") String date_end) {
+        
+        System.out.println("id: " +id_sis+ ", start: " +date_start+ ", end: "+date_end);
+        String date_start_str = date_start.replace("\"", "").trim();
+        String date_end_str = date_end.replace("\"", "").trim();
+
+        Optional<Sistema> sis = sistemaService.getSisById(id_sis);
+
+        List<Registos> regSisList = registoService.getRegBySis(sis);
+        List<Registos> regStartList = registoService.getByStartDate(regSisList, date_start_str);
+        List<Registos> regEndList = registoService.getByEndDate(regStartList, date_end_str);
+        
+        List<RegistoRequest> request_list = new ArrayList<>();
+
+        Iterator<Registos> iterator = regEndList.iterator();
+        while (iterator.hasNext()) {
+            Registos reg = iterator.next();
+            if(reg.getType() == "Cons"){
+                RegistoRequest reg_req = new RegistoRequest(reg.getEnergia(), reg.getTime_init(), reg.getTime_final(), reg.getType());
+                request_list.add(reg_req);
+            }
+
         }
 
         return  new ResponseEntity<>(request_list,HttpStatus.OK);
